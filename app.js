3bcf8ecd45dee0cbd2730de08972702102d5144c
Valeria Astorga <valeriauniversityacc@gmail.com>
2026-04-05 15:43:22 -0600

    Inicializa estructura base del proyecto Dale Click

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/emprendedor/tablero.html'));
+});
+
+module.exports = app;
